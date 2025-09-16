7987acc382c6d7c2bdc8ff483435a601e47f3c98
AurenForge <amlewis1216@gmail.com>
2025-09-16 12:25:49 -0500

    Delete hybrid-trainer-pwa-v2-repack directory

--- hybrid-trainer-pwa-v2-repack/service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE='hybrid-trainer-v2-repack';
-const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
